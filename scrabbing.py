b8c3112c45823448cf0d786b27e263c2b75792c4
Nalaxon <bernhard.rapp@student.tugraz.at>
2019-11-03 17:19:02 +0100

    playground code for sending messages

--- scrabbing.py
+++ scrabbing.py
@@ -13,6 +13,8 @@ def read_config():
 config = read_config()['foodsoft']
 request_url = config['url'] + 'login'
 login_url = config['url'] + 'sessions'
+request_message_url = config['url'] + 'messages/new?message%5Bmail_to%5D=315'
+send_message_url = config['url'] + 'messages'
 
 with requests.Session() as s:
     #headers={"Content-Type":"application/x-www-form-urlencoded",
@@ -35,10 +37,37 @@ with requests.Session() as s:
             'password' : config['password'],
             'commit' : 'Anmelden'}
     login_data['authenticity_token'] = html.find(attrs={'name': 'authenticity_token'})['value']
+    print(login_data['authenticity_token'])
     response = s.post(login_url, headers = headers, data = login_data, cookies=r1.cookies)
-    print(response.status_code)
-#pp.pprint(vars(raw_html))
-#print(raw_html.content)
+    print('Response send login (POST): ' + str(response.status_code))
+    r1 = s.get(request_message_url, headers=headers)
+    print('Response message form (GET): ' + str(r1.status_code))
+    html = BeautifulSoup(r1.content, 'html.parser')
+    token = html.find(attrs={'name':'csrf-token'})['content']
+    print(token)
+    message = {
+            'utf8': '%25E2%259C%2593',
+            #'authenticity_token':str(token),
+            'message[reply_to]':'',
+            'message[send_methode]':'recipients',
+            'message[workgroup_id]':'1',
+            'message[ordergroup_id]':'162',
+            'message[order_id]':'1472',
+            'message[recipient_tokens]':'315',
+            'message[private]':['0','1'],
+            'message[subject]':'test-subject',
+            'message[body]':'test',
+            'commit':'Nachricht+verschicken'
+            }
+    headers['Referer'] = request_message_url
+    response = s.post(send_message_url, headers=headers, data=message, cookies=r1.cookies)
+    #print(html.contents)
+    print('Response send message (POST): ' + str(response.status_code))
+   
+
+
+    #pp.pprint(vars(html))
+    #print(html.contents)
 #print(len(raw_html))
 #html = BeautifulSoup(raw_html, 'html.parser')
 #print(html.select('title'))
